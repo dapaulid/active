8fc9d41d3fdd1a8f611c294417068404390b158c
Daniel Pauli <dapaulid@gmail.com>
2021-06-06 11:11:44 +0200

    Build and run example instead of active file.

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@ echo Running developer command prompt...
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo Building...
-cl.exe %*
+cl.exe /Zi /EHsc /Fe:example\example.exe example\example.cpp
 if %errorlevel% neq 0 exit /b %errorlevel%
 echo Build successful.
